8b3421745fae43aa0167363f62edc535cc51c017
yein <gkshs1234@gmail.com>
2018-05-24 19:40:05 +0900

    우선 구글 로그인을 통해서만 가입할 수 있도록 완료

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Upgrade  Dutchpay application
 이전과 다른점 
 - 파이어베이스 Authentication, Database 사용
 - 네트워크와 ui 로직 구분(?)
+
+
+
+수정한 부분
+
+- 메인화면을 로그인 화면이 아님
+  - Google로그인 or 메일
+
--- app/src/main/java/yein/dutchpay/LoginActivity.java
+++ app/src/main/java/yein/dutchpay/LoginActivity.java
@@ -1,13 +1,124 @@
 package yein.dutchpay;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "GoogleActivity";
+    private static final int RC_SIGN_IN = 9001;
+
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        findViewById(R.id.btn_google_signIn).setOnClickListener(this);
+        findViewById(R.id.btn_join_with_mail).setOnClickListener(this);
+        findViewById(R.id.btn_login).setOnClickListener(this);
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    // Activity 시작시
+    @Override
+    public void onStart() {
+        super.onStart();
+        // 이미 로그인된 유저일 경우, stateActivity로 바로 이동
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null ){
+            startActivity(new Intent(LoginActivity.this, StateActivity.class));
+            finish();
+        }
+    }
+
+    private void GoogleSignIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success");
+                    startActivity(new Intent(LoginActivity.this, StateActivity.class));
+                    finish();
+                    //FirebaseUser user = mAuth.getCurrentUser();
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.d(TAG, "signInWithCredential:failure", task.getException());
+                    Snackbar.make(findViewById(R.id.activity_login), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
+        if(i == R.id.btn_google_signIn ){
+            GoogleSignIn();
+       // }else if(i == R.id.btn_join_with_mail){
+            // 구글 이메일 외의 메일을 이용해 가입하는 JoinActivity 시작
+         //   startActivity(new Intent(LoginActivity.this, JoinActivity.class));
+        }else if(i == R.id.btn_login){
+            // 이미 계정이 있을 경우 Login
+        }
     }
 }
